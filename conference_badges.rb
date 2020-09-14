cac62e8d812233980a7cc4087c705b497630ceec
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-14 22:26:52 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+
+def batch_badge_creator(attendees)
+  new_array = [""]                                                          
+  
+  attendees.each do |attendee|                                              
+    new_array <<  "Hello, my name is #{attendee}."                         
+    badge_maker = [""]                              
+  end                                                                     
+  
+  badges                                                                         
+end 
